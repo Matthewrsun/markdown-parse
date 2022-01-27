224488f2bee914da967e55ecabef34cdc77b686f
Matthewrsun <matthewrsun06@gmail.com>
2022-01-26 16:30:44 -0800

    Added JUnit tests for MarkdownParse

---
+++ MarkdownParseTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.*;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testFile() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void break1() throws IOException{
+        Path fileName = Path.of("break1.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void break2() throws IOException{
+        Path fileName = Path.of("break2.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something().com"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void break3() throws IOException{
+        Path fileName = Path.of("break3.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    }
+}
